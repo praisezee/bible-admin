42d4dc5d4d6108c4e6dc2117ffd4a231bcb6dbcf
dev_apus <folorunsopraise12@gmail.com>
2025-09-24 09:40:09 +0100

    done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -111,14 +111,14 @@ export default function LoginPage() {
 						</Button>
 					</form>
 
-					<div className="mt-4 text-center text-sm">
+					{/* <div className="mt-4 text-center text-sm">
 						<span className="text-muted-foreground">Don't have an account? </span>
 						<Link
 							href="/signup"
 							className="text-primary hover:underline font-medium">
 							Sign up
 						</Link>
-					</div>
+					</div> */}
 				</CardContent>
 			</Card>
 		</div>
